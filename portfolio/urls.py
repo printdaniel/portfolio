d49a222617aea07c9644f423fc708a19df880b72
Daniel <dwsuarez98@gmail.com>
2023-08-07 16:16:45 -0300

    saqué el about y mejoré el home

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('home.urls')),
-    path("", include('about.urls')),
     path("", include('projects.urls')),
     path("", include('contact.urls')),
 ]
