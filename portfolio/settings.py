8c83455f013370db0baddcb9dff8019c8ed0abda
Daniel <dwsuarez98@gmail.com>
2023-08-09 15:56:22 -0300

    Navbar arreglado, y template project

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles" 
 
--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,5 @@ from .models import Project, ProjectMedia
 def projects(request):
     projects = Project.objects.all()
     media = ProjectMedia.objects.all()
-
-
     context = {'projects': projects, 'media':media}
     return render(request, 'projects/projects.html', context)
