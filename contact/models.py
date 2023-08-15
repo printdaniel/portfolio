b3a5dba8396ac28c2681402c7da6b35f7008a27a
Daniel <dwsuarez98@gmail.com>
2023-08-15 14:02:08 -0300

    formulario contacto

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    mensaje = models.TextField()
+
+
+    def __str__(self):
+        return self.nombre
